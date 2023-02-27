b73619657f5a6f5e98a9261417818353452967e9
frenchface <37390855+frenchface@users.noreply.github.com>
2023-02-27 14:25:17 -0500

    Update ldapauth.sh

--- ldapauth.sh
+++ ldapauth.sh
@@ -1,16 +1,9 @@
-#varibles                                            ldapauth.sh                                                               
-domain="@home.int"
-ldapip=10.50.0.2
-base="dc=home,dc=int"
-
-
-#do not modify below this line
-
+domain="@<enter your infor here>"
 user=$username$domain
 if [ -z "$username" ]; then exit 1; fi
 if [ -z "$password" ]; then exit 1; fi
 #echo $user
-search1=$(ldapsearch -H ldap://$ldapip -x -w "$password" -D $user -b "$base" "(samAccountName=$username)" | grep search:)
+search1=$(ldapsearch -H ldap://<yourLDAPServerHere> -x -w "$password" -D $user -b "dc=<enter your infor here>,dc=<enter your infor here>" "(samAccountName=$username)" | grep search:)
 #echo $search1
 if [ -z "$search1" ]; then echo "NULL"; else echo "User '$username' authenticated successfully."; fi
 if [ -z "$search1" ]; then echo "NULL"; else echo "name = $username"; fi
