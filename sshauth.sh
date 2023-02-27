7b7b2fcb863d7d7cf98b5abd373014e6ee9119e0
frenchface <37390855+frenchface@users.noreply.github.com>
2023-02-27 14:27:33 -0500

    Update sshauth.sh

--- sshauth.sh
+++ sshauth.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
-#variables
-IP=192.168.1.2 #IP Address of remote LDAP authentication host
-
-#Do not modify below this line
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-ssh -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa root@$IP &>> /config/log "username=$username password='$password' bash ./ldapauth.sh"
+
+ssh -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa root@<yourIPhere> &>> /config/log "username=$username password='$password' bash ./ldapauth.sh"
+
 exit $x
